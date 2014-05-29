bf845fec52765fb8a6568eb12244c86d93ab3d00
Virginia Dooley <virginia@ideavine.org>
2014-05-29 16:42:03 -0400

    Create java.js

---
+++ Homework-May24/java.js
@@ -0,0 +1,80 @@
+//Javascript cheat sheet
+
+//Data types
+
+	//Strings
+
+	("Hello NYCDA");
+
+	//Numbers
+
+	5
+
+	//Booleans
+
+	2 < 1;
+	"I am taller than you".length>10  ;
+
+	//Undefined
+
+	var = x; if (x = Undefined) {"x is Undefined"} ;
+
+//Debugging
+
+	//Alerts
+
+	alert ("Hi Sam");
+
+	//Comments
+
+	/* This is a multi-
+	line
+	comment */
+
+	//console.log
+
+	console.log ("you are logged in");
+
+//Arrays
+
+	//Multi-dimensional arrays
+
+	var colors = ['turquoise', 'navy', 'teal'];
+	var shapes = ['circle', 'square', 'star'];
+	console.log; (var colorsshapes [0][0]);
+	>"turquoise" "circle";
+
+//Testing
+
+	//Equal values
+
+	var x = 5; 
+	var y = 5;
+	x+y;
+	>10
+
+	//Equal value AND Data type
+
+	if(10>1) {colors = "navy"};
+	else {colors = "teal"};
+
+	// Not equal to
+
+	var x = 2;
+	var y = 4; 
+
+
+//Operations
+
+	//Addition
+
+	1 + 5
+
+	//Subtraction
+
+	10 - 5
+
+	//Multiply
+
+	3 * 3
+
